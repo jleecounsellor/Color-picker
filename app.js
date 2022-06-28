e7f293b89df22528f77ff8e4f52bc30ac0678543
Jamie Counsellor <46204858+jleecounsellor@users.noreply.github.com>
2022-06-27 20:27:30 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E","F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function() {
+  let hexColor = "#";
+
+  for (let i = 0; i < 6; i++){
+    const randomNum = getRandomNum();
+    hexColor += hex[randomNum];
+  }
+
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
+
+function getRandomNum() {
+  //get values for length of array
+  return Math.floor(Math.random() * hex.length);
+}
